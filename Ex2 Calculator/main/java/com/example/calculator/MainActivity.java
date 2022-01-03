3f96db513f6b993e5505fd683ce1bd6800dc01a6
sushnesh01 <93457835+sushnesh@users.noreply.github.com>
2022-01-03 11:01:25 +0530

    Update MainActivity.java

--- Ex2 Calculator/main/java/com/example/calculator/MainActivity.java
+++ Ex2 Calculator/main/java/com/example/calculator/MainActivity.java
@@ -59,8 +59,8 @@ public class MainActivity extends AppCompatActivity {
         workingsTV.setText(workings);
     }
 
-    public void onclickPower(View view) {
-        setWorkings("^");
+    public void onclickMod(View view) {
+        setWorkings("%");
     }
 
     public void onclickDiv(View view) {
@@ -82,9 +82,9 @@ public class MainActivity extends AppCompatActivity {
     public void onclickEqual(View view) {
         Double result = null;
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
-        checkForPowerOf();
+
         try {
-            result = (double) engine.eval(formula);
+            result = (double) engine.eval(workings);
 
         } catch (ScriptException e) {
             Toast.makeText(this, "Invalid Input", Toast.LENGTH_SHORT).show();
@@ -94,58 +94,6 @@ public class MainActivity extends AppCompatActivity {
             workingsTV.setText(String.format("%.2f", result));
             workings = String.valueOf(result.doubleValue());
         }
-    }
-
-    private void checkForPowerOf()
-    {
-        ArrayList<Integer> indexOfPowers = new ArrayList<>();
-        for(int i = 0; i < workings.length(); i++)
-        {
-            if (workings.charAt(i) == '^')
-                indexOfPowers.add(i);
-        }
-
-        formula = workings;
-        tempFormula = workings;
-        for(Integer index: indexOfPowers)
-        {
-            changeFormula(index);
-        }
-        formula = tempFormula;
-    }
-
-    private void changeFormula(Integer index)
-    {
-        String numberLeft = "";
-        String numberRight = "";
-
-        for(int i = index + 1; i< workings.length(); i++)
-        {
-            if(isNumeric(workings.charAt(i)))
-                numberRight = numberRight + workings.charAt(i);
-            else
-                break;
-        }
-
-        for(int i = index - 1; i >= 0; i--)
-        {
-            if(isNumeric(workings.charAt(i)))
-                numberLeft = numberLeft + workings.charAt(i);
-            else
-                break;
-        }
-
-        String original = numberLeft + "^" + numberRight;
-        String changed = "Math.pow("+numberLeft+","+numberRight+")";
-        tempFormula = tempFormula.replace(original,changed);
-    }
-
-    private boolean isNumeric(char c)
-    {
-        if((c <= '9' && c >= '0') || c == '.')
-            return true;
-
-        return false;
     }
 
     public void onclickNumber2(View view) {
